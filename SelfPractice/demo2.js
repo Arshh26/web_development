8975f9ae8dd1b6a6fb0d5423b1afae055fe3c4a8
Arshh26 <arshadali99936@gmail.com>
2026-07-24 16:19:31 +0530

    Scope Variable,Datatypes,typeof

--- SelfPractice/demo2.js
+++ SelfPractice/demo2.js
@@ -1,4 +1,25 @@
-console.log("This is a separate js file");
-let n = 10;
-let m = 12;
-console.log(n + m);
+let company = "OpenAI"; //this is a global variable
+console.log(typeof company);
+
+function display() {
+  console.log(company);
+}
+display();
+
+console.log(company); //it can be accessed  from anywhere
+
+// Local Variable
+
+function name() {
+  let myName = "Arshad";
+  console.log(myName);
+}
+name();
+
+console.log(myName);
+
+let age = 22;
+console.log(typeof age);
+
+let num = 1000000000000000000000000000000000000000000;
+console.log(typeof num);
